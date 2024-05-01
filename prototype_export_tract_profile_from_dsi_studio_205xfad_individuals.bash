eade379f94b322fb6a87d5b34ea10deb81666242
Harrison Mansour <harrisonmelman@gmail.com>
2024-04-30 21:04:09 -0400

    initial commit

---
+++ prototype_export_tract_profile_from_dsi_studio_205xfad_individuals.bash
@@ -0,0 +1,79 @@
+# load fib file into DSI Studio
+# load tract file in
+# run Tracts -> TractProfile ... ->
+    # data sampling strategu = fiber orientation
+    # quantitative index = QA (Al thinks that FA would be a much more interesting metric to look at, but dsi studio just shows a blank line there in the plot?)
+    # save report data ...
+        # save as...
+
+
+# DEOCUEMNTATNION FROM DSI Studio
+# Tract Functions
+# Parameters	Description
+# tract	Specify the tract file
+# output	Use”–output=Tract.txt” to convert the trk file to another format or ROI (assigned output file as NIFTI file)
+# export	Export additional information related to the fiber tracts
+# Export tract density images
+
+# Use --export=tdi to generate a track density image in the diffusion space. To output in x2, x3, x4 resolution, use tdi2, tdi3, tdi4 (also applied to the following commands).
+# Use --export=tdi_color to generate a track color density image.
+# Use --export=tdi_end to output only the endpoint.
+# Export tract profile
+
+# Use --export=stat to export tract statistics like along tract mean fa, adc, or morphology index such as volume, length, … etc.
+# Use --export=report:dti_fa:0:1 to export the tract reports on “fa” values with a profile style at x-direction “0” and a bandwidth of “1”
+# The profile style can be the following:
+
+# 0: x-direction
+# 1: y-direction
+# 2: z-direction
+# 3: along tracts
+# 4: mean of each tract
+# You can export multiple outputs separated by “,”. For example,
+# --export=stat,tdi,tdi2 exports track statistics, tract density images (TDI), subvoxel TDI, along tract qa values, and along tract gfa values.
+
+# example export Functions
+# dsi_studio --action=ana --source=*.gqi.1.25.fib.gz --export=qa,iso,dti_fa,rd,ad
+
+#dsi_studio="K:/CIVM_Apps/dsi_studio_64/dsi_studio_win_v2023-03-28/dsi_studio.exe";
+dsi_studio="//pwp-civm-ctx01/K/CIVM_Apps/dsi_studio_64/dsi_studio_win_v2023-03-28/dsi_studio.exe";
+
+# load fib data
+# export tract profile of QA metric, with sampling style 3="along tracts", and with bandwidth=1
+#fib_file="B:/22.gaj.49/DMBA/Aligned-Data-RAS/Other/QSDR_fib/N58211NLSAM/nii4D_N58211NLSAM.odf.15um_mdt.qsdr.0.6.R96.fib.gz";
+tract_file="B:/ProjectSpace/hmm56/prototype_dsi_studio_TBSS/20.5xfad.01_AD_BxD77/whole_brain_0.6_test_region_0.tt.gz";
+contrast_list="dti_fa ad"
+bandwidth=1;
+sample_strategy=3;
+
+strain_list="all_Ntg all_tg";
+
+
+for strain in $strain_list; do
+    input_dir="B:/20.5xfad.01/QSDR/BXD77/average_volumes/${strain}";
+    for fib_file in $(ls ${input_dir}/nii4d*fib.gz); do
+        fib_file=${fib_file%%@};
+        fib_file=$(readlink $fib_file);
+        runno=$(basename ${fib_file});
+        runno=${runno%%.src*};
+        runno=${runno##nii4d_};
+        for contrast in $contrast_list; do
+            out_prefix="B:/ProjectSpace/hmm56/prototype_dsi_studio_TBSS/20.5xfad.01_AD_BxD77/threshold_0.6_experiment_0_individuals/${runno}_${strain##all_}_${contrast}";
+            ${dsi_studio} --action=ana --source=${fib_file} --tract=${tract_file} --export=report:${contrast}:${sample_strategy}:${bandwidth} --output=${out_prefix};
+            #exit 1;
+        done
+    done
+done
+
+exit 1;
+# old code
+for x in N58211 N58646 N58656 N58981 N59007; do
+    fib_file="B:/22.gaj.49/DMBA/Aligned-Data-RAS/Other/QSDR_fib/${x}NLSAM/nii4D_${x}*fib.gz";
+    out_prefix="B:/ProjectSpace/hmm56/prototype_dsi_studio_TBSS/20.5xfad.01_AD_BxD77/0.6_region_0_individuals/${x}";
+    ${dsi_studio} --action=ana --source=${fib_file} --tract=${tract_file} --export=report:${contrast}:${sample_strategy}:${bandwidth} --output=${out_prefix};
+done
+
+
+fib_file="B:/22.gaj.49/DMBA/Aligned-Data-RAS/Other/QSDR_fib/template_mean/template.mean.fib.gz";
+out_prefix="B:/ProjectSpace/hmm56/prototype_dsi_studio_TBSS/DMBA_comparative/template_whole_brain_track/threshold_0.6_experiment_0/template";
+${dsi_studio} --action=ana --source=${fib_file} --tract=${tract_file} --export=report:${contrast}:${sample_strategy}:${bandwidth} --output=$out_prefix;
---
+++ prototype_plot_tract_profile.m
@@ -0,0 +1,145 @@
+% exploring the DSI Studio tract profile functionality
+% exported the tract profile data into
+    % B:\ProjectSpace\hmm56\prototype_dsi_studio_TBSS\DMBA_comparative\template_whole_brain_track\threshold_0.6_experiment_0
+    % here, tracking was performed on DMBA template.mean.fib.gz
+        % whole brain tracking
+        % other parameters
+    % then a small tract through X region was selected, This is a very 
+    % strong WM bundle
+        % this was exported and loaded in to all 5 template fib files 
+        % N58211, N58646, N58656, N58981, N59007
+        % and the tract profile was saved along this fiber bundle for each
+% in DSI Studio, the only visualization provided is a simple plot, one
+% specimen at a time. 
+% here, I seek to improve that by plotting the tract profile for each
+% individual and the template in the same plot.
+% I can also include confidence intervals
+
+% understanding the tract profile output file:
+% tab separated value txt file 
+% first column for both rowss is "headers", just the name of the track file
+% profile was pulled from
+% first row is index, 0...99
+% second row is [values 0...99] "CI" [confidence interval lower bounds 
+% 0...99] "CI" [confidence interval upper bounds 0...99] 
+% unsure if the bundle is always broken up into 100 bands, or if it depends
+% on length
+
+
+
+%% 20.5xfad.01 bxd77 testing
+% read data from file
+contrast = 'AD';
+in_dir = 'B:\ProjectSpace\hmm56\prototype_dsi_studio_TBSS\20.5xfad.01_AD_BxD77\0.6_region_0';
+figure; hold on;
+runnos = {'all_ntg', 'all_tg'};
+color = {'k', 'g', 'b', 'c', 'm', 'r'};
+%color = {'k', 'r', 'r', 'r', 'r', 'r'};
+for i=1:length(runnos)
+    % TODO: variables/arguments for report filename pattern
+    %in_file = strcat(in_dir, '\', runnos{i}, '_report.txt');
+    % auto-exported filename ex: N58211.report.dti_fa.3.1.txt
+    in_file = strcat(in_dir, '\', runnos{i}, '_', contrast, '_report.txt');
+    [x, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report(in_file);
+    %[x, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report_4row(in_file);
+    %if i==1
+        plot_one_confidence_interval(x, y_CI_min, y_CI_max, color{i});
+    %end
+    plot_one_profile(x, y, color{i});
+end
+legend('ntg-CI', 'ntg', 'tg-CI', 'tg');
+title(contrast);
+
+hold off;
+%% DMBA testing
+%{
+% read data from file
+in_dir = 'B:\ProjectSpace\hmm56\prototype_dsi_studio_TBSS\DMBA_comparative\template_whole_brain_track\threshold_0.6_experiment_0';
+figure; hold on;
+runnos = {'template', 'N58211', 'N58646', 'N58656', 'N58981', 'N59007'};
+color = {'k', 'g', 'b', 'c', 'm', 'r'};
+%color = {'k', 'r', 'r', 'r', 'r', 'r'};
+for i=1:6
+    % TODO: variables/arguments for report filename pattern
+    %in_file = strcat(in_dir, '\', runnos{i}, '_report.txt');
+    % auto-exported filename ex: N58211.report.dti_fa.3.1.txt
+    in_file = strcat(in_dir, '\', runnos{i}, '.report.dti_fa.3.1.txt');
+    %[x, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report(in_file);
+    [x, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report_4row(in_file);
+    if i==1
+        plot_one_confidence_interval(x, y_CI_min, y_CI_max, color{i});
+    end
+    plot_one_profile(x, y, color{i});
+end
+%legend('template-CI', 'template', 'N58211-CI', 'N58211', 'N58646-CI', 'N58646', 'N58656-CI', 'N58656', 'N58981-CI', 'N58981', 'N59007-CI', 'N59007');
+legend('template 95% CI', 'template', 'N58211', 'N58646', 'N58656', 'N58981', 'N59007');
+
+hold off;
+%}
+
+%% functions
+
+% ugh, fuck dSI STUDIO
+% use this one if you manually export from the GUI, and use the other one
+% fi you extract profile reports from command line....jeez
+function [x, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report(in_file)
+    A = readtable(in_file, delimiter='\t');
+    % first row is the indices, start at column 2 because 1st is a title
+    % second row is the y values, then upper and lower CI's, split these up
+    x = A(1,2:101);
+    y = A(2,2:101);
+    y_CI_min = A(2,103:202);
+    y_CI_max = A(2,204:303);
+    % convert all from table to array
+    x = table2array(x);
+    y = table2array(y);
+    y_CI_min = table2array(y_CI_min);
+    y_CI_max = table2array(y_CI_max);
+    % convert all from row to column vectors
+    % this was NOT NECESSARY
+    %x = x';
+    %y = y';
+    %y_CI_min = y_CI_min';
+    %y_CI_max = y_CI_max';
+end
+
+function [x, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report_4row(in_file)
+    A = readtable(in_file, delimiter='\t');
+    % first row is the indices, start at column 2 because 1st is a title
+    % second row is the y values, then upper and lower CI's, split these up
+    x = A(1,2:101);
+    y = A(2,2:101);
+    %y_CI_min = A(2,103:202);
+    %y_CI_max = A(2,204:303);
+    y_CI_min = A(3,2:101);
+    y_CI_max = A(4,2:101);
+    % convert all from table to array
+    x = table2array(x);
+    y = table2array(y);
+    y_CI_min = table2array(y_CI_min);
+    y_CI_max = table2array(y_CI_max);
+    % convert all from row to column vectors
+    % this was NOT NECESSARY
+    %x = x';
+    %y = y';
+    %y_CI_min = y_CI_min';
+    %y_CI_max = y_CI_max';
+end
+
+function plot_one_profile(x, y, color)
+    % just draw confidence bounds as a dotted line
+    value_format = strcat(color, '*');
+    plot(x,y,value_format); 
+end
+
+function plot_one_confidence_interval(x, y_CI_min, y_CI_max, color)
+    % concat upper and lower bounds CI into a 2xn column vector
+    % stolen from https://www.mathworks.com/help/matlab/creating_plots/line-plot-with-confidence-bounds.html
+    x_CI = [x x(end:-1:1)];
+    y_CI = [y_CI_min y_CI_max(end:-1:1)];
+    p = fill(x_CI,y_CI,color);
+    p.FaceAlpha = 0.1;
+    p.FaceColor = color;
+    %p.EdgeColor = 'none';
+    p.EdgeColor = color;
+end
