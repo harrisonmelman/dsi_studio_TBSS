f7c39800067b07a6d1e3e743de8894b283b1631c
Harrison Mansour <harrisonmelman@gmail.com>
2024-06-17 10:08:53 -0400

    experiment csv report is now exported and saved to a tab-delimited csv. Each row is one  time series. includes columns for gene_status, runno, contrast for filtering

--- prototype_plot_tract_profile_individuals_205xfad01_multiplot.m
+++ prototype_plot_tract_profile_individuals_205xfad01_multiplot.m
@@ -54,7 +54,9 @@ color = {'k', 'g', 'b', 'c', 'm', 'r'};
 % then loop through plot all TG in green
 
 full_runno_list = [ntg_runno_list, tg_runno_list];
-[column_names, data_csv] = make_multicontrast_csv(ntg_runno_list, 'Ntg_all', tg_runno_list, 'tg_all', contrast_list, in_dir, cli_export);
+out_file = 'B:\ProjectSpace\hmm56\prototype_dsi_studio_TBSS\prototype_matlab_csv_export.txt';
+[column_names, data_csv] = make_multicontrast_csv(out_file, ntg_runno_list, 'Ntg_all', tg_runno_list, 'tg_all', contrast_list, in_dir, cli_export);
+
 %make_multicontrast_figure(group1, group1_name, group2, group2_name, contrast_list, in_dir, cli_export)
 
 %% functions
@@ -67,7 +69,7 @@ full_runno_list = [ntg_runno_list, tg_runno_list];
 % $runno_$contrast_CI_lower,
 % $runno_$contrast_CI_upper, 
 % group information (gene_status, age, sex, strain, etc)
-function [column_names, data_csv] = make_multicontrast_csv(group1, group1_name, group2, group2_name, contrast_list, in_dir, cli_export)
+function [column_names, data_csv] = make_multicontrast_csv(out_file, group1, group1_name, group2, group2_name, contrast_list, in_dir, cli_export)
     % estimate csv size for preallocation
     % 100 data points, plus columns for gene_status, runno, contrast
     % add more columns later for more refined groupings
@@ -75,7 +77,8 @@ function [column_names, data_csv] = make_multicontrast_csv(group1, group1_name, 
     % TIMES 3 if you include the confidence intervals. keep off for test
     num_rows = 1 * (length(group1) + length(group2)) * length(contrast_list);
     %data_csv = zeros(num_rows,num_cols);
-    data_csv = [];
+    % use a cell array to put both numerical and categorical data in
+    data_csv = {};
     row_names = {};
     column_names = {'name', 'runno', 'contrast', 'group'};
     % TODO: fix this so that you don't copy and paste the for loop twice
@@ -93,6 +96,8 @@ function [column_names, data_csv] = make_multicontrast_csv(group1, group1_name, 
             %in_file = strcat(in_dir, '\', runno, '_', group_name, '_', contrast, '.report.', contrast, '.3.1.txt');
             in_file = strcat(in_dir, '\', runno, '.report.', contrast, '.3.1.txt');
             [~, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report_4row(in_file);
+            y = num2cell(y);
+            y = [in_file, runno, contrast, group_name, y];
             % for testing, just the simple values
             %new_columns = {strcat(runno,'_',contrast,'_val'), strcat(runno,'_',contrast,'_CI_lower'), strcat(runno,'_',contrast,'_CI_upper')};
             new_rows = {strcat(runno,'_',contrast,'_val')};
@@ -106,11 +111,16 @@ function [column_names, data_csv] = make_multicontrast_csv(group1, group1_name, 
             runno = runno_list{j};
             in_file = strcat(in_dir, '\', runno, '.report.', contrast, '.3.1.txt');
             [x, y, y_CI_min, y_CI_max] = extract_values_and_CI_from_dsi_studio_tract_profile_report_4row(in_file);
+            y = num2cell(y);
+            y = [in_file, runno, contrast, group_name, y];
             new_rows = {strcat(runno,'_',contrast,'_val')};
             row_names = [row_names, new_rows];
             data_csv = [data_csv; y];
         end
     end
+
+    % write cell array to file
+    writecell(data_csv, out_file, 'Delimiter','tab');
 end
 
 % loop through contrast list
